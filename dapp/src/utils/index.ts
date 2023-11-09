bbc9fdb2d54676f98a6bac82610d789d65ee0e36
arnasalex <arnas@noia.network>
2023-11-09 18:18:28 +0200

    fix: deadcode issue

--- dapp/src/utils/index.ts
+++ dapp/src/utils/index.ts
@@ -1,4 +1,4 @@
+export * from './date';
+export { getAccessToken, getEnv, getSubjectName, isMockedApi } from './env';
 export * from './format';
 export * from './numbers';
-export * from './env';
-export * from './date';
